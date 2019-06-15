139ce09103be254e13af2465e3254b283e042fd5
Asus <mihajlojelenic@gmail.com>
2019-06-15 19:20:09 +0200

    domaci2

--- CetvrtaGrupa/mihajloJelenic.py
+++ CetvrtaGrupa/mihajloJelenic.py
@@ -1,15 +1,22 @@
-osobe=[]
-nastavi="d"
+# osobe=[]
+# nastavi="d"
 
-while nastavi =="d":
-	ime=input("Unesite ime: ")
-	prez=input("Unesite prezime: ")a
-	nastavi=input("Nastavi: d/n? ")
-	osobe.append((ime,prez))
+# while nastavi =="d":
+	# ime=input("Unesite ime: ")
+	# prez=input("Unesite prezime: ")a
+	# nastavi=input("Nastavi: d/n? ")
+	# osobe.append((ime,prez))
 	
-filter=input("Unesite slova za filtriranje imena: ")
+# filter=input("Unesite slova za filtriranje imena: ")
 	
-print(osobe)
+# print(osobe)
 
-filtriranaLista=[x for x in osobe if filter in x[0]]
-print(filtriranaLista)
+# filtriranaLista=[x for x in osobe if filter in x[0]]
+# print(filtriranaLista)
+
+class osoba:
+	def __init__(self,ime,prezime):
+		self.ime=ime
+		self.prezime=prezime
+	def inicijali (self):
+		return self.ime[0]+self.prezime[0]
