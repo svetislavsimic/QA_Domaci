720815122403a889c3b0264574414e4293fc9d18
Aleksandra Aleksic <aleksandra.aleksic150192@gmail.com>
2019-06-27 10:47:10 +0200

    Promena boje polja na kojem je pokazivac

--- CetvrtaGrupa/primenaStilova.py
+++ CetvrtaGrupa/primenaStilova.py
@@ -3,10 +3,16 @@ import tkinter.ttk as ttk
 
 root = tk.Tk()
 
+root.title("Lista brojeva")
+
 style = ttk.Style()
 style.configure("BW.TButton", foreground="black", background="white",
-                padding=20, relief="flat")
+                padding=10, relief="flat", activebackground="yellow")
+
+style.map('BW.TButton', foreground = [('active', '!disabled', 'green')],
+                     background = [('active', 'red')])
 for x in range(10):
-    b=ttk.Button(text=x, style="BW.TButton").pack()
+    b=ttk.Button(text=x, style="BW.TButton", ).pack()
+
 
 root.mainloop()
