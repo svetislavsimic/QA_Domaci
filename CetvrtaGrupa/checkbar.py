9e7612b350eb98e890cddb02f34d8b2630792cb0
Ivan Petric <petroman90@gmail.com>
2019-06-30 17:06:37 +0200

    dodat prikaz odabranih polja

--- CetvrtaGrupa/checkbar.py
+++ CetvrtaGrupa/checkbar.py
@@ -5,6 +5,7 @@ class Checkbar(ttk.Frame):
    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
       Frame.__init__(self, parent)
       self.vars = []
+      self.picks=picks
       for pick in picks:
          var = IntVar()
          chk = ttk.Checkbutton(self, text=pick, variable=var)
@@ -12,18 +13,30 @@ class Checkbar(ttk.Frame):
          self.vars.append(var)
    def state(self):
         return [var.get() for var in self.vars]
-        
+   def stateRepresent(self):
+      picked = [p[0] for p in list(zip(self.picks,self.vars)) if p[1].get()==1]
+      return picked
 if __name__ == '__main__':
    root = Tk()
    lng = Checkbar(root, ['Python', 'Ruby', 'Perl', 'C++'])
    tgl = Checkbar(root, ['English','German','Spain'])
+   hobi = Checkbar(root, ['Sport', 'Muzika', 'Pecanje'])
    lng.pack(side=TOP,  fill=X)
-   tgl.pack(side=LEFT)
+   tgl.pack(side=TOP,fill=X)
+   hobi.pack(side=TOP,fill=X)
    lng.config(relief=GROOVE, bd=2)
+   tgl.config(relief=GROOVE, bd=2)
+   hobi.config(relief=GROOVE, bd=2)
 
    def allstates(): 
       print(list(lng.state()), list(tgl.state()))
-   
+      print(Checkbar.stateRepresent(lng),Checkbar.stateRepresent(tgl),Checkbar.stateRepresent(hobi))
+      lb1= Label(root,text="Izabrali ste: " + str(Checkbar.stateRepresent(lng)))
+      lb2 = Label(root, text="Izabrali ste: " + str(Checkbar.stateRepresent(tgl)))
+      lb3 = Label(root, text="Izabrali ste: " + str(Checkbar.stateRepresent(hobi)))
+      lb1.pack(side=RIGHT)
+      lb2.pack(side=RIGHT)
+      lb3.pack(side=RIGHT)
    #*******
    s = ttk.Style()
    
