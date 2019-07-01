d311d0f8b4abad7aa72e74d6f57984e678cf77fb
sima <svetislavsimic77@gmail.com>
2019-07-01 20:35:47 +0200

    Save file

--- CetvrtaGrupa/MyNotepad/MainWindow.py
+++ CetvrtaGrupa/MyNotepad/MainWindow.py
@@ -36,7 +36,7 @@ class App:
         # create a pulldown menu, and add it to the menu bar
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open", command=self.Open)
-        #filemenu.add_command(label="Save", command=self.Save)
+        filemenu.add_command(label="Save", command=self.Save)
 
         #filemenu.add_command(label="Rename", command=self.Rename)
         filemenu.add_separator()
@@ -44,7 +44,7 @@ class App:
         menubar.add_cascade(label="File",  menu=filemenu)
         fontMenu=tk.Menu(menubar, tearoff=1)
         fontMenu.add_command(label="IncreaseFont", command=self.IncreaseFont)
-        #fontMenu.add_command(label="DecreaseFont", command=self.DecreaseFont)
+        fontMenu.add_command(label="DecreaseFont", command=self.DecreaseFont)
         menubar.add_cascade(label="Font", menu=fontMenu)
         searchMenu= tk.Menu(menubar, tearoff= 0)
         #searchMenu.add_command(label="Count", command = self.count)
@@ -73,5 +73,15 @@ class App:
             return
         with open(self.filename,mode='r') as f:
             self.editArea.insert('1.0', f.read())
+
+    def Save(self):
+        f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+            return
+        print(f)
+        print(type(f))
+        with f:
+            text2save = str(self.editArea.get(1.0, "end"))  # starts from `1.0`, not `0.0`
+            f.write(text2save)
 
 app=App()
