1c56549e4ef74195f7b989b1943461e654aca2f6
sima <svetislavsimic77@gmail.com>
2019-07-01 18:58:25 +0200

    aded  My_Notepad

---
+++ CetvrtaGrupa/MyNotepad/MainWindow.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import tkinter.scrolledtext as tkst
+import tkinter.font as tkFont
+from tkinter import messagebox
+from tkinter import filedialog
+import os
+import shutil
+
+class App:
+    def __init__(self,filename=None):
+        self.win = tk.Tk()
+        #self.win.protocol("WM_DELETE_WINDOW", self.Exit)
+        self.customFont = tkFont.Font(
+            family="Helvetica", size=14
+        )
+        self.filename=filename
+        self.query = tk.StringVar()
+        frame1 = tk.Frame(
+            master=self.win,
+             background = 'red'
+        )
+        frame1.pack(fill='both', expand='yes')
+        self.editArea = tkst.ScrolledText(
+            master=frame1,
+            wrap=tk.WORD,
+            font=self.customFont,
+        )
+
+        self.editArea.pack(padx=3, pady=3, fill=tk.BOTH, expand=True)
+        self.editArea.focus_set()
+
+        menubar = tk.Menu(self.win)
+        helpmenu = tk.Menu(menubar, tearoff=0)
+        #helpmenu.add_command(label="?", command=self.Help)
+
+        # create a pulldown menu, and add it to the menu bar
+        filemenu = tk.Menu(menubar, tearoff=0)
+        #filemenu.add_command(label="Open", command=self.Open)
+        #filemenu.add_command(label="Save", command=self.Save)
+
+        #filemenu.add_command(label="Rename", command=self.Rename)
+        filemenu.add_separator()
+        #filemenu.add_command(label="Exit", command=self.Exit)
+        menubar.add_cascade(label="File",  menu=filemenu)
+        fontMenu=tk.Menu(menubar, tearoff=1)
+        #fontMenu.add_command(label="IncreaseFont", command=self.IncreaseFont)
+        #fontMenu.add_command(label="DecreaseFont", command=self.DecreaseFont)
+        menubar.add_cascade(label="Font", menu=fontMenu)
+        searchMenu= tk.Menu(menubar, tearoff= 0)
+        #searchMenu.add_command(label="Count", command = self.count)
+        menubar.add_cascade(label="Search", menu=searchMenu )
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+        self.win.config(menu=menubar)
+
+
+        self.win.mainloop()
+
+app=App()
--- CetvrtaGrupa/checkbar.py
+++ CetvrtaGrupa/checkbar.py
@@ -5,6 +5,7 @@ class Checkbar(ttk.Frame):
    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
       Frame.__init__(self, parent)
       self.vars = []
+      self.picks = picks
       for pick in picks:
          var = IntVar()
          chk = ttk.Checkbutton(self, text=pick, variable=var)
@@ -12,6 +13,10 @@ class Checkbar(ttk.Frame):
          self.vars.append(var)
    def state(self):
         return [var.get() for var in self.vars]
+
+   def stateRepresent(self):
+      picked = [p[0] for p in zip(self.picks,self.vars) if p[1].get()==1]
+      return picked
         
 if __name__ == '__main__':
    root = Tk()
@@ -22,7 +27,7 @@ if __name__ == '__main__':
    lng.config(relief=GROOVE, bd=2)
 
    def allstates(): 
-      print(list(lng.state()), list(tgl.state()))
+      print(list(lng.stateRepresent()), list(tgl.stateRepresent()))
    
    #*******
    s = ttk.Style()
